4ec6b3e617b2328beacf0869eb623124155c5f62
Ash Maher <ashkanmaherbus@gmail.com>
2017-07-15 14:01:57 -0700

    final product

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -4,5 +4,18 @@ $(document).ready(function() {
 	$(".brownBtn").on("click", function() {
 		$(".brownContainer").show();
 	});
+$(".scream").hover(function() {
+	$(".scream").attr("src", "assets/images/291.png")
+	$(".scream").animate({
+		height: "1000px"
+   });
+  
+  }, function() {
+	$(".scream").attr("src", "assets/images/290.gif")
+  	
+  	$(".scream").animate({
+  		height: "100%"
 
+  	});
+  });
 });
